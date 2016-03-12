33a48e8feb837c71232439b79a04dfc34d0d80db
Walpitagamage Binuri Mandula <g3tdirty@cdf.toronto.edu>
2016-03-11 20:57:05 -0500

    heap dataStructure and mm_init method

--- allocators/a2alloc/a2alloc.c
+++ allocators/a2alloc/a2alloc.c
@@ -1,20 +1,60 @@
 #include <stdlib.h>
 #include "memlib.h"
+#include "mm_thread.h"
+#include "malloc.h"
+
+/* *** DATA STRUCTURES USED *** */
+typedef struct {
+    int used_bytes;
+    int allocated_bytes;
+    pthread_mutex_t mta_lock;
+} heap_t;
+
+
+/* *** VARIABLES NEEDED *** */
+static heap_t *heaps = NULL;
+static heap_t *global_heap = NULL;
 
 void *mm_malloc(size_t sz)
 {
-	(void)sz; /* Avoid warning about unused variable */
-	return NULL;
+    (void)sz; /* Avoid warning about unused variable */
+    return NULL;
 }
 
 void mm_free(void *ptr)
 {
-	(void)ptr; /* Avoid warning about unused variable */
+    (void)ptr; /* Avoid warning about unused variable */
 }
 
 
 int mm_init(void)
 {
-	return 0;
+
+    /* initalize memory if dseg_lo/hi is not set */
+    if (dseg_lo == NULL && dseg_hi == NULL) {
+        int success = mem_init();
+        if (success == -1){
+            return success;
+        }
+
+
+        heaps = (heap_t *)mem_sbrk(mem_pagesize());
+
+        int i = 0;
+        for (i = 0; i <= getNumProcessors(); i++){
+            heap_t *heap = &heaps[i];
+
+            pthread_mutexattr_t mta;
+            pthread_mutexattr_init(&mta);
+
+            pthread_mutex_init(&(heap->mta_lock), &mta);
+        }
+    }
+
+    /* global heap is at heaps[0] */
+    global_heap = heaps;
+
+    return 0;
 }
+
 
